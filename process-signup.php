53a07f4396b0a5543fc3ee063b8f100f340edc00
Joshuaomwami <omwamijoshua913@gmail.com>
2023-05-27 16:55:38 +0300

    second Commit

--- process-signup.php
+++ process-signup.php
@@ -1,31 +1,35 @@
 <?php
-// Establish a connection to your database
+// Database connection details
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "medmaster_db";
+$database = "medmaster_db";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
 
-// Check for connection errors
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve the form data
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-// Insert the user details into the users table
-$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "User registered successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    // Insert user details into the database
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    if ($conn->query($sql) === TRUE) {
+        // Successful sign-up
+        echo "<div class='success'>Sign up successful. Redirecting to log-in page...</div>";
+        echo "<script>setTimeout(function() { window.location.href = 'log-in.php'; }, 3000);</script>";
+        exit();
+    } else {
+        // Error occurred while inserting data
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
-// Close the database connection
 $conn->close();
 ?>

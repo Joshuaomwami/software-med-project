c1d7c0ecb814e875f9597db0b73d4224f03a7228
Joshuaomwami <omwamijoshua913@gmail.com>
2023-05-28 16:46:41 +0300

    second commit

---
+++ password-reset.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Password Reset</title>
+  <link rel="stylesheet" href="password-reset.css">
+</head>
+
+<body>
+  <div class="container">
+    <h1>Password Reset</h1>
+    <p>Enter your email address below to reset your password.</p>
+    <form id="password-reset-form" method="POST" action="process-password-reset.php">
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+      </div>
+      <button type="submit" name="submit">Reset Password</button>
+    </form>
+    <p>Remembered your password? <a href="login.php">Login here</a></p>
+  </div>
+</body>
+
+</html>

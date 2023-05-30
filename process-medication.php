f20a7da6efc3e38d84be7dbd09e2782bce7ea611
Joshuaomwami <omwamijoshua913@gmail.com>
2023-05-30 12:46:40 +0300

    second commit

--- process-medication.php
+++ process-medication.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $endDate = $_POST["end-date"];
 
     // Insert the medication data into the database
-    $sql = "INSERT INTO medication (user_id, medication_name, dosage, frequency, start_date, end_date) VALUES (?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO medication (medication_name, dosage, frequency, begin_date, end_date) VALUES ($medicationName,$dosage,$frequency,$startDate,$endDate )";
 
     // Prepare the statement
     $stmt = $conn->prepare($sql);
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Execute the statement
     if ($stmt->execute()) {
         // Medication data inserted successfully, redirect to the medication list page
-        header("Location: medication-list.php");
+        header("Location: medication-list.html");
         exit();
     } else {
         // Error occurred while inserting data, display an error message
@@ -50,3 +50,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Close the connection
 $conn->close();
 ?>
+
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "medmaster_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['submit'])) {
+    $medicationName = $_POST['medication-name'];
+    $dosage = $_POST['dosage'];
+    $frequency = $_POST['frequency'];
+    $startDate = $_POST['start-date'];
+    $endDate = $_POST['end-date'];
+
+    // Insert medication details into the database
+    $sql = "INSERT INTO medication (medication_name, dosage, frequency, begin_date, end_date) VALUES ('$medicationName', '$dosage', '$frequency', '$startDate', '$endDate')";
+    if ($conn->query($sql) === TRUE) {
+        // Successful insertion
+        echo "<script>alert('Medication added successfully.'); window.location.href = 'medication-list.html';</script>";
+        exit();
+    } else {
+        // Error occurred while inserting data
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+

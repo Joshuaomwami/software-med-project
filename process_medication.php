a8f3476c16d8d14893ca2a1e1ad7d8fb956992db
Joshuaomwami <omwamijoshua913@gmail.com>
2023-06-04 17:35:35 +0300

    second commit

--- process_medication.php
+++ process_medication.php
@@ -1,41 +1,41 @@
 <?php
-  session_start(); 
+session_start();
 
-  $medicationName = $_POST['medication_name'];
-  $dosage = $_POST['dosage'];
-  $frequency = $_POST['frequency'];
-  $startDate = $_POST['start-date'];
-  $endDate = $_POST['end-date'];
+$medicationName = $_POST['medication_name'];
+$dosage = $_POST['dosage'];
+$frequency = $_POST['frequency'];
+$startDate = $_POST['start-date'];
+$endDate = $_POST['end-date'];
 
 // Retrieve the user ID from the session variable
-  $userID = $_SESSION['user_id'];
-  
-  // Database connection details
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $database = "medmaster_db";
-  
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $database);
-  
-  // Check connection
-  if (mysqli_connect_error()) {
+$userID = $_SESSION['user_id'];
+
+// Database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "medmaster_db";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (mysqli_connect_error()) {
     die("Connection failed: " . mysqli_connect_error());
-  }
-  
-  // Insert medication details into the database
-  $sql = "INSERT INTO medication (medication_name, dosage, frequency, begin_date, end_date) 
-  VALUES ('$medicationName', '$dosage', '$frequency', '$beginDate', '$endDate')";
-  if (mysqli_query($conn, $sql)) {
-    echo"Successful medication addition";
+}
+
+// Insert medication details into the database
+$sql = "INSERT INTO medication (user_id, medication_name, dosage, frequency, start_date, end_date) 
+        VALUES ('$userID', '$medicationName', '$dosage', '$frequency', '$startDate', '$endDate')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Successful medication addition";
     header("Location: medication-list.php");
     exit();
-  } else {
-    echo"Error occurred while inserting data";
-    echo "Error: " . $sql . "<br>" . mysqli_error($sql);
-  }
-  
-  mysqli_close($conn);
+} else {
+    echo "Error occurred while inserting data";
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
+mysqli_close($conn);
 ?>

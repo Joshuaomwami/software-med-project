95ee95df0342429324badd56d5c6f6e001ebdc14
Joshuaomwami <omwamijoshua913@gmail.com>
2023-06-04 18:27:46 +0300

    second commit

--- add_medication.php
+++ add_medication.php
@@ -1,3 +1,54 @@
+<?php
+session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // User not logged in, redirect to login page
+    header("Location: log-in.php");
+    exit();
+}
+
+// Get the user ID from the session
+$userID = $_SESSION['user_id'];
+
+if (isset($_POST['submit'])) {
+    $medicationName = $_POST['medication_name'];
+    $dosage = $_POST['dosage'];
+    $frequency = $_POST['frequency'];
+    $startDate = $_POST['start-date'];
+    $endDate = $_POST['end-date'];
+
+    // Database connection details
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "medmaster_db";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    // Check connection
+    if (mysqli_connect_error()) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Insert medication details into the database
+    $sql = "INSERT INTO medication (user_id, medication_name, dosage, frequency, begin_date, end_date) 
+            VALUES ('$userID', '$medicationName', '$dosage', '$frequency', '$startDate', '$endDate')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Successful medication addition";
+        header("Location: medication-list.php");
+        exit();
+    } else {
+        echo "Error occurred while inserting data";
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,7 +68,7 @@
   </nav>
   <div class="container">
     <h1>MedMaster Reminder</h1>
-    <form id="form" method="POST" action="process_medication.php">
+    <form id="form" method="POST" action="add_medication.php">
       <div class="form-group">
         <label for="medication-name">Medication Name</label>
         <input type="text" id="medication-name" name="medication_name" required>
@@ -44,7 +95,6 @@
       </div>
     
       <button type="submit" id="add-medication" name="submit">Add Medication</button>
-
     </form>
   </div>
 </body>
